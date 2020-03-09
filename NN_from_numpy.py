e366f87c9047009726efd39e01c55a3cc2d26dd5
ASzot <me@andrewszot.com>
2020-03-08 21:51:19 -0700

    network trains successfully

--- NN_from_numpy.py
+++ NN_from_numpy.py
@@ -1,29 +1,33 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar  8 10:01:05 2020
-
-@author: noahforman
-"""
-from __future__ import absolute_import, division, print_function, unicode_literals
- 
 import numpy as np
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from load_dat import get_scans, pre_process_scans
 
-
-train_dataset = tf.data.Dataset.from_tensor_slices(( , )) #(train_examples, train_labels)
-test_dataset = tf.data.Dataset.from_tensor_slices(( , )) #(test_examples, test_labels)
-
+# LOCATION OF THE FOLDERS CONTAINING THE DICOM FILES.
+DATA_FOLDER = 'data/dicom/'
+NUM_CLASSES = 10
 BATCH_SIZE = 64
 SHUFFLE_BUFFER_SIZE = 100
+
+scans = get_scans(DATA_FOLDER)
+scans = pre_process_scans(scans)
+labels = np.random.randint(0, NUM_CLASSES, len(scans))
+
+# For now just work with a single slice in the scan
+scans = scans[:, 0]
+
+x_train, x_test, y_train, y_test = train_test_split(scans, labels, test_size=0.2)
+
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
 train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
 test_dataset = test_dataset.batch(BATCH_SIZE)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Flatten(input_shape=(100, 100)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense() # This will need to change depending on the number of classes we have.
+    tf.keras.layers.Dense(10)
 ])
 
 model.compile(optimizer=tf.keras.optimizers.RMSprop(),
--- load_dat.py
+++ load_dat.py
@@ -2,48 +2,52 @@ import pydicom
 import os
 import os.path as osp
 import numpy as np
-
-# LOCATION OF THE FOLDERS CONTAINING THE DICOM FILES.
-DATA_FOLDER = 'data/dicom/'
+import cv2
 
 
-dcms = []
-for dcm_folder in os.listdir(DATA_FOLDER):
-    patient_dcm = []
-    for f in os.listdir(osp.join(DATA_FOLDER, dcm_folder)):
-        full_f = osp.join(DATA_FOLDER, dcm_folder, f)
-        dcm_slice = pydicom.read_file(full_f)
-        patient_dcm.append(dcm_slice)
-    dcms.append(patient_dcm)
+def get_scans(data_loc):
+    dcms = []
+    for dcm_folder in os.listdir(data_loc):
+        patient_dcm = []
+        for f in os.listdir(osp.join(data_loc, dcm_folder)):
+            full_f = osp.join(data_loc, dcm_folder, f)
+            dcm_slice = pydicom.read_file(full_f)
+            patient_dcm.append(dcm_slice)
+        dcms.append(patient_dcm)
 
-dcm_np = [[dcm.pixel_array for dcm in patient_dcm] for patient_dcm in dcms]
+    dcm_np = [[dcm.pixel_array for dcm in patient_dcm] for patient_dcm in dcms]
 
-min_dims = np.min([[x.shape
-    for x in patient_dcm]
-    for patient_dcm in dcm_np], axis=-1)[0]
+    min_dims = np.min([[x.shape
+        for x in patient_dcm]
+        for patient_dcm in dcm_np], axis=-1)[0]
 
-# This will need to change because some patients only have one scan?
-min_scans = min([len(x) for x in dcm_np])
+    # This will need to change because some patients only have one scan?
+    min_scans = min([len(x) for x in dcm_np])
 
-dcm_np = np.array([[x[:min_dims[0],:min_dims[1]]
-    for x in patient_dcm[:min_scans]]
-    for patient_dcm in dcm_np])
-
-
-# (num patients, number of scans per patient, width, length)
-n, d, w, l = dcm_np.shape
-
-# The crop dimensions around the center of the image.
-CROP_DIM = (300, 200)
-
-# Crop
-hl = l // 2
-hw = w // 2
-cropped_dcms = dcm_np[:, :, hw-(CROP_DIM[0]//2):hw+(CROP_DIM[0]//2), hl-(CROP_DIM[1]//2):hl+(CROP_DIM[1]//2)]
+    dcm_np = np.array([[x[:min_dims[0],:min_dims[1]]
+        for x in patient_dcm[:min_scans]]
+        for patient_dcm in dcm_np])
+    return dcm_np
 
 
-# Normalize
-normalized_dc = (cropped_dcms - np.mean(cropped_dcms, axis=0)) / np.std(cropped_dcms, axis=0)
+def pre_process_scans(dcm_np):
+    # (num patients, number of scans per patient, width, length)
+    n, d, w, l = dcm_np.shape
+
+    # The crop dimensions around the center of the image.
+    CROP_DIM = (300, 200)
+
+    # Crop
+    hl = l // 2
+    hw = w // 2
+    cropped_dcms = dcm_np[:, :, hw-(CROP_DIM[0]//2):hw+(CROP_DIM[0]//2), hl-(CROP_DIM[1]//2):hl+(CROP_DIM[1]//2)]
+
+    # Normalize
+    normalized_dc = (cropped_dcms - np.mean(cropped_dcms, axis=0)) / np.std(cropped_dcms, axis=0)
+    resized_dc = np.array([[cv2.resize(x, (100, 100))
+            for x in patient_dcm]
+            for patient_dcm in normalized_dc])
+    return resized_dc
 
 
 
