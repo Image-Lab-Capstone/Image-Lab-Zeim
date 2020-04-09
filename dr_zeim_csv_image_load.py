8f20f5f99fcedf1b7b2b52218ea279fa3270c0c4
Kieran Agarwal <kaagarwa@usc.edu>
2020-04-08 21:52:52 -0500

    made some changes, now has complete dataframe as output

--- dr_zeim_csv_image_load.py
+++ dr_zeim_csv_image_load.py
@@ -1,21 +1,41 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sat Mar 28 17:37:55 2020
-Loading data from excel into python data
-structures for Dr. Zeim
+Loading input data from excel from Dr. Zeim
+and converting to pandas dataframe with 
+binary labels for each of the possible labels
 """
 
-import numpy as np
 import pandas as pd
+pd.options.display.max_columns = 50
 
-def read_data(filepath, images):
-    csv_dataframe = pd.read_csv(filepath)
-    return csv_dataframe
-    
-    
+#Set filepaths
+input_data_path = 'sample/zeim_input.csv'
+label_dictionary_path = 'sample/labels.csv'
+output_path = 'sample/output.csv'
 
-if __name__ == '__main__':
-    df = read_data('input_data/dr_zeim_input.csv', 'input_data/images')
-    names = df['Filename']
-    labels = df['Label']
-    isTraining = df['Train_Image']
+#Read data set and label list
+df = pd.read_csv(input_data_path)
+labels = pd.read_csv(label_dictionary_path)
+
+#Set arrays
+file_names = df['Image Index']
+file_labels = df['Finding Labels']
+label_dict = labels['Finding Label']
+isTraining = df['Train Image']
+
+#Create binary column for each classification
+for label in label_dict:
+    new_label_column = []
+    for classifications in file_labels:
+        if label in classifications:
+            new_label_column.append(1)
+        else:
+            new_label_column.append(0)
+    df[label] = new_label_column
+    
+#Export dataframe to csv
+df.to_csv (output_path, index = False, header=True)
+
+            
+
+
+
